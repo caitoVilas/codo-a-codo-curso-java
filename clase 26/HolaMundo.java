15368d9ca14f41dae230dfdad4f95d18569cedf8
caito <caitocd@gmail.com>
2020-11-05 16:44:10 -0300

    primera clase java codo codo

---
+++ clase 26/HolaMundo.java
@@ -0,0 +1,14 @@
+public class HolaMundo {
+	
+	public static void main(String[] args){
+
+		String name = "Caito";
+	        int age = 15;
+
+		System.out.println("Hola mundo");
+		System.out.println("usando java a la antigua");
+		System.out.println("Mi nombre es : " + name);
+		System.out.println("Tengo : " + age);
+
+      }
+}
